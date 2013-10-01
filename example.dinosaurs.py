6349329a6937c0c999affdeda0248cde6df2a298
Leonard Richardson <leonardr@segfault.org>
2013-09-30 22:08:16 -0400

    Added dinosaur names (courtesy Rob Dubbin/Wikipedia) and an example script for generating dinosaur names.

---
+++ example.dinosaurs.py
@@ -0,0 +1,16 @@
+import json
+from queneau import WordAssembler
+from data import load_json
+import textwrap
+assembler = WordAssembler(load_json("dinosaurs.json"))
+
+dinos = []
+for i in range(2):
+    dino = assembler.assemble_word()
+    if dino[0] in ['AEIO']:
+        dino = "an " + dino
+    else:
+        dino = "a " + dino
+    dinos.append(dino)
+
+print "Look! Behind that ridge! It's %s fighting %s!" % tuple(dinos)
