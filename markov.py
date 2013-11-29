abbec27c538d4b5c0498424be4b2e963f4010a2c
Leonard Richardson <leonardr@segfault.org>
2013-11-29 16:35:18 -0500

    Added a Markov generator that tried to keep brackets and quotes balanced.

--- markov.py
+++ markov.py
@@ -27,6 +27,9 @@ class MarkovGenerator(object):
     def loadlines(cls, f, order=1, max=500):
         """Load from a filehandle that defines one text per line."""
         corpus = MarkovGenerator(n, max)
+        if not hasattr(f, 'read'):
+            # Not a file-type object. Treat it as a multi-line string.
+            f = f.split("\n")
         for l in f:
             corpus.add(l.strip())
         return corpus
@@ -79,7 +82,7 @@ class MarkovGenerator(object):
                     # This is the final item!
                     done = True
                     break
-                yield next
+                yield self.modify(next)
                 output.append(next)
                 # get the last N entries of the output; we'll use this to look up
                 # an ngram in the next iteration of the loop
@@ -87,7 +90,68 @@ class MarkovGenerator(object):
             else:
                 break
 
+    generate = assemble
     chain = assemble
+
+    def modify(self, token):
+        """Modify a token before yielding it."""
+        return token
+
+class BracketMatchingMarkovGenerator(MarkovGenerator):
+
+    """A generator that tries to ensure balanced brackets and double quotes.
+
+    It's not perfect, but it's a lot better than nothing.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(BracketMatchingMarkovGenerator, self).__init__(*args, **kwargs)
+        self.useful_tokens = set([])
+
+    def tokenize(self, text):
+        tokens = super(BracketMatchingMarkovGenerator, self).tokenize(text)
+        for token in tokens:
+            for closing in ['"', ")", "]", "}"]:
+                if token.endswith(closing):
+                    self.useful_tokens.add(token)
+        return tokens
+
+    def assemble(self):
+        self.stack = []
+        for x in super(BracketMatchingMarkovGenerator, self).assemble():
+            yield x
+
+    def modify(self, token_to_yield):
+        # Is there an opening bracket in this token?
+        for opening, closing in ['""', "()", "[]", "{}"]:
+            if opening in token_to_yield:
+                index = token_to_yield.find(opening)
+                if token_to_yield.find(closing, index+1) == -1:
+                    self.stack.append(closing)
+            elif closing in token_to_yield:
+                # We are closing a bracket. Is there an open bracket?
+                if len(self.stack) == 0:
+                    #Nope.
+                    token_to_yield = token_to_yield.replace(closing, "")
+                elif closing != self.stack[-1]:
+                    # Force it to be the right kind of bracket.
+                    token_to_yield = token_to_yield.replace(
+                        closing, self.stack[-1], 1)
+                    self.stack.pop()
+                else:
+                    # It's already the right kind of bracket.
+                    self.stack.pop()
+
+        if len(self.stack) > 0:
+            # If we modify this token by appending the closing bracket
+            # on top of the stack, would we get a token that exists in
+            # the corpus?
+            check_for = token_to_yield + self.stack[-1]
+            if check_for in self.useful_tokens:
+                self.stack.pop()
+                return check_for
+        # No luck. Return the original token.
+        return token_to_yield
   
 if __name__ == '__main__':
     import sys
