36c297f8a9bcb87dfef67c572f939555ecc5092c
Leonard Richardson <leonardr@segfault.org>
2013-12-01 21:22:44 -0500

    Added a port of the word filter.

---
+++ wordfilter.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+dir = os.path.split(__file__)[0]
+
+BLACKLIST = json.load(open(os.path.join(dir, "data", "badwords.json")))['badwords']
+
+def is_blacklisted(string, blacklist=BLACKLIST):
+    s = string.lower()
+    for slur in blacklist:
+        if slur in s:
+            return True
+    return False
