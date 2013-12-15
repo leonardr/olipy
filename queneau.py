5858e738708646e43dffa98e86e7ebc7ae30ef11
Leonard Richardson <leonardr@segfault.org>
2013-12-15 10:58:16 -0500

    Added a SentenceAssembler to queneau.

--- queneau.py
+++ queneau.py
@@ -193,7 +193,20 @@ class Assembler(object):
                     continue
             f.write(json.dumps(item) + "\n")
 
+class SentenceAssembler(Assembler):
+    """Assemble sentences from words.
+
+    Markov chains are usually better for this."""
+
+    WHITESPACE = re.compile("\s+")
+
+    def add(self, item):
+        words = self.WHITESPACE.split(item)
+        super(SentenceAssembler, self).add(words)
+
 class WordAssembler(Assembler):
+
+    """Assemble words from runs of vowels and consonants."""
 
     def __init__(self, initial=[]):
         self.vowel_runs_by_position = self.empty_bucket()
